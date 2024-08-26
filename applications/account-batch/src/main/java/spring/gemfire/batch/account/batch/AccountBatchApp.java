41d178b3c11ac4a4873948912a5ecb71b8b41b3e
Govind Gaikwad <govindrgaikwad@gmail.com>
2024-08-26 17:27:52 -0600

    changes to account batch for prometheous

--- applications/account-batch/src/main/java/spring/gemfire/batch/account/batch/AccountBatchApp.java
+++ applications/account-batch/src/main/java/spring/gemfire/batch/account/batch/AccountBatchApp.java
@@ -3,13 +3,14 @@ package spring.gemfire.batch.account.batch;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.micrometer.prometheus.rsocket.PrometheusRSocketClient;
 import io.rsocket.transport.netty.client.TcpClientTransport;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.listener.annotation.AfterTask;
 import org.springframework.cloud.task.repository.TaskExecution;
 @SpringBootApplication
-public class AccountBatchApp {
+public class AccountBatchApp implements DisposableBean {
 
 	@Autowired
 	PrometheusMeterRegistry prometheusMeterRegistry;
@@ -25,13 +26,14 @@ public class AccountBatchApp {
 	@AfterTask
 	public void afterTask(TaskExecution taskExecution) {
 
-		System.out.println(prometheusMeterRegistry.scrape());
-		client.pushAndClose();
-
 		client = PrometheusRSocketClient
 				.build(prometheusMeterRegistry, TcpClientTransport.create("35.223.217.203", 7001))
 				.connect();
 		client.pushAndClose();
 	}
 
+	@Override
+	public void destroy() throws Exception {
+		client.pushAndClose();
+	}
 }
